80c0f497e73366099ae8ca195ca4e5a7ed5b7ddc
Dimitrij Pinneker <dimapin@googlemail.com>
2016-03-18 14:19:42 +0100

    Filter job names by jobPrefix

--- src/main/groovy/com/neoteric/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/neoteric/jenkins/JenkinsJobManager.groovy
@@ -47,9 +47,9 @@ class JenkinsJobManager {
 		println "-------------------------------------"
 		println "All branch names:" + allBranchNames
 
-		List<String> allJobNames = jenkinsApi.jobNames
+		List<String> allJobNames = jenkinsApi.jobNames(jobPrefix)
 		println "-------------------------------------"
-		println "All job names:" + allJobNames
+		println "All job names with prefix "+jobPrefix+":" + allJobNames
 
 		List<TemplateJob> templateJobs = findRequiredTemplateJobs(allJobNames)
 		println "-------------------------------------"
